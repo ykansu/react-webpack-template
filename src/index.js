62f0bfbf002622254db830cf1a77a82ca6452e4e
ykansu <yasinkansu3@gmail.com>
2019-10-09 23:02:17 +0300

    fist commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import FormContainer from "./js/components/container/FormContainer.jsx";
+import React from "react"
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<FormContainer />, document.getElementById("root")) ;
