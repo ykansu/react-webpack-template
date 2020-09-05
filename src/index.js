c4e3df5ed62ef43a29ed46af2aa79fff23cdcda9
Yasin KANSU <yasinkansu3@gmail.com>
2020-09-05 15:30:33 +0300

    master reverted

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(<FormContainer />, document.getElementById("root")) ;
 
-
+//master reverted
 if (module.hot) {
     // Whenever a new version of App.js is available
     module.hot.accept('./js/components/container/FormContainer.jsx', function () {
